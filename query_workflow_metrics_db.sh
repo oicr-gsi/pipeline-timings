8dae4dc14b81ca8b17b79327a28f4798307f701d
anallan <anallan@oicr.on.ca>
2025-03-05 09:25:32 -0500

    update on output directory

--- query_workflow_metrics_db.sh
+++ query_workflow_metrics_db.sh
@@ -4,17 +4,14 @@
 module load mongodb-database-tools/100.5.2 
 module load jq
 
-# Specify output directory for .JSON files 
-output_dir = "Extracted_Metrics"
-
 # Create output directory
-mkdir -p "$output_dir"
+mkdir Extracted_Metrics
 
 while IFS= read -r line; do 
    
   query_str='{"workflow_run_id": "'"$line"'"}' 
   echo $query_str
-  time mongoexport --host workflow-metrics-db.gsi.oicr.on.ca --port 27017 --username workflow_metrics_ro --config /.mounts/labs/gsi/secrets/workflow-metrics-db.gsi_workflow_metrics_ro --db workflow_metrics --collection production_cromwell_workflow_metrics --jsonArray --query "$query_str"| jq "." > "$output_dir/$line.json"
+  time mongoexport --host workflow-metrics-db.gsi.oicr.on.ca --port 27017 --username workflow_metrics_ro --config /.mounts/labs/gsi/secrets/workflow-metrics-db.gsi_workflow_metrics_ro --db workflow_metrics --collection production_cromwell_workflow_metrics --jsonArray --query "$query_str"| jq "." > "Extracted_Metrics/$line.json"
 
 
 done < workflow_ids.txt
